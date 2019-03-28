283b9f5395133bca45bf2094c872b549146049af
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-03-29 11:34:04 +1300

    Update deflate.c

--- deflate.c
+++ deflate.c
@@ -66,7 +66,7 @@ const char deflate_copyright[] =
 typedef enum {
     need_more,      /* block not completed, need more input or more output. */
     block_done,     /* block flush performed. */
-    finish_started, /* finish started, need only more output at next deflate */
+    finish_started, /* finish started, need only more output at next deflate. */
     finish_done     /* finish done, accept no more input or output */
 } block_state;
 
